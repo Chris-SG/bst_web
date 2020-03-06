b0a804e5103237334f7285ba0e4d9ec437135cb0
Chris-SG <christopher.shandgost@myob.com>
2020-03-06 22:50:28 +1100

    Test route

--- router_external.go
+++ router_external.go
@@ -1,15 +1,42 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gorilla/mux"
 	"github.com/urfave/negroni"
+	"net/http"
+	"strings"
 )
 
 func CreateExternalRouters(middleware map[string]*negroni.Negroni) *mux.Router {
 	externalRouter := mux.NewRouter().PathPrefix("/external").Subrouter()
 
-	externalRouter.PathPrefix("/external/bst_api").Handler(negroni.New(
+	externalRouter.PathPrefix("/bst_api").Handler(negroni.New(
 		negroni.Wrap(CreateBstApiRouter(middleware))))
 
+	externalRouter.Path("/status").Handler(negroni.New(
+		negroni.Wrap(http.HandlerFunc(StatusGet))))
+
+
+	fmt.Println("-----WALKING ROUTES-----")
+	externalRouter.Walk(walk)
+	fmt.Println("-----FINISHED WALK-----")
+
 	return externalRouter
+}
+
+func walk(route *mux.Route, router *mux.Router, ancestors []*mux.Route) error {
+	pathTemplate, err := route.GetPathTemplate()
+	if err == nil {
+		fmt.Println("ROUTE:", pathTemplate)
+	}
+	pathRegexp, err := route.GetPathRegexp()
+	if err == nil {
+		fmt.Println("Path regexp:", pathRegexp)
+	}
+	if !strings.HasSuffix(pathRegexp, "$") {
+		route.Subrouter().Walk(walk)
+	}
+	fmt.Println()
+	return nil
 }
--- server.go
+++ server.go
@@ -67,33 +67,6 @@ func main() {
 		negroni.HandlerFunc(RedirectHomeMiddleware),
 		negroni.Wrap(http.HandlerFunc(IndexHandler(staticDirectory + indexPage)))))
 
-	fmt.Println("-----WALKING ROUTES-----")
-	r.Walk(func(route *mux.Route, router *mux.Router, ancestors []*mux.Route) error {
-		pathTemplate, err := route.GetPathTemplate()
-		if err == nil {
-			fmt.Println("ROUTE:", pathTemplate)
-		}
-		pathRegexp, err := route.GetPathRegexp()
-		if err == nil {
-			fmt.Println("Path regexp:", pathRegexp)
-		}
-		queriesTemplates, err := route.GetQueriesTemplates()
-		if err == nil {
-			fmt.Println("Queries templates:", strings.Join(queriesTemplates, ","))
-		}
-		queriesRegexps, err := route.GetQueriesRegexp()
-		if err == nil {
-			fmt.Println("Queries regexps:", strings.Join(queriesRegexps, ","))
-		}
-		methods, err := route.GetMethods()
-		if err == nil {
-			fmt.Println("Methods:", strings.Join(methods, ","))
-		}
-		fmt.Println()
-		return nil
-	})
-	fmt.Println("-----FINISHED WALK-----")
-
 	var certManager *autocert.Manager
 
 	certManager = &autocert.Manager{
