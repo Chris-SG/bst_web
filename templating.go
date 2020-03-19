71128e8c5c6e64f107b62f492ff2aff6189d5ccd
Chris-SG <christopher.shandgost@myob.com>
2020-03-19 18:02:41 +1100

    CSS changes

--- templating.go
+++ templating.go
@@ -80,7 +80,9 @@ func LoadCommonScripts() string {
 func LoadCommonSheets() string {
 	return `
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="/css/common.css">`
+    <link rel="stylesheet" href="/css/common.css">
+	<link rel="stylesheet" href="/css/header.css">
+    <link rel="stylesheet" href="/css/footer.css">`
 }
 
 func LoadEagateLogin(user string) {
